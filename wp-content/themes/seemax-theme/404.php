13bea1b328fcb079be6f72e13da197982fe8943c
Max Mearsheimer <max@seemaxwork.com>
2018-10-23 16:45:10 -0500

    Remove 404 Page Loop Healdine Style Fix

--- wp-content/themes/seemax-theme/404.php
+++ wp-content/themes/seemax-theme/404.php
@@ -1,17 +1,14 @@
 <?php /* Template Name: 404 Page */ get_header(); ?>
 <main class="404-page">
-    <?php while (have_posts()) : the_post(); ?>
-        <section class="main-section">
-            <div class="content">
-              <h1><?php the_field('404_error_heading','option'); ?></h1>
-              <h2><?php the_field('404_error_subheading','option'); ?></h2>
-              <div class="button">
-                <a href="<?php echo site_url();?>"></a>
-                <?php the_field('404_error_button_text','option'); ?></a>
-              </div>
-            </div>
-        </section>
-    <?php endwhile; ?>
+  <section class="main-section">
+      <div class="content">
+        <h1><?php the_field('404_error_heading','option'); ?></h1>
+        <h2><?php the_field('404_error_subheading','option'); ?></h2>
+        <div class="button">
+          <a href="<?php echo site_url();?>"></a>
+          <?php the_field('404_error_button_text','option'); ?></a>
+        </div>
+      </div>
+  </section>
 </main>
 <?php get_footer(); ?>
-

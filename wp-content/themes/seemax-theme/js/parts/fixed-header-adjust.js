2698c06ad5a5251102a469e42ee824f2e5ce6960
Max Mearsheimer <max@seemaxwork.com>
2018-12-10 15:17:48 -0600

    Create fixed-header-adjust.js

---
+++ wp-content/themes/seemax-theme/js/parts/fixed-header-adjust.js
@@ -0,0 +1,20 @@
+// UNCOMMENT FOR FIXED HEADER
+(function fixedHeaderAdjust() {
+
+  // // Move the Main Section down based on header height
+  // function headerPadding() {
+  //   var tl = new TimelineMax();
+  //   var headerHeight = $('.header').height();
+  //
+  //   tl.set($('main'), {marginTop:headerHeight});
+  // }
+  //
+  // // Set Up The Main Section Padding on Load
+  // headerPadding();
+  //
+  // // Readjust main padding when window is resized
+  // $(window).on('resize', function(){
+  //   headerPadding();
+  // });
+
+}());

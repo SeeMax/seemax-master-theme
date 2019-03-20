cf2b6339f972ab569ff2e4e4c5057850a70f1e0f
Max Mearsheimer <max@seemaxwork.com>
2019-03-20 16:39:00 -0500

    404 and index.php updates

--- wp-content/themes/seemax-theme/404.php
+++ wp-content/themes/seemax-theme/404.php
@@ -1,11 +1,11 @@
 <?php /* Template Name: 404 Page */ get_header(); ?>
-<main class="404-page">
+<main class="four-oh-four-page">
   <section class="main-section">
       <div class="content">
         <h1><?php the_field('404_error_heading','option'); ?></h1>
-        <h2><?php the_field('404_error_subheading','option'); ?></h2>
+        <h3><?php the_field('404_error_subheading','option'); ?></h3>
         <div class="button">
-          <a href="<?php echo site_url();?>"></a>
+          <a class="c-block-fill" href="<?php echo site_url();?>"></a>
           <?php the_field('404_error_button_text','option'); ?></a>
         </div>
       </div>
--- wp-content/themes/seemax-theme/index.php
+++ wp-content/themes/seemax-theme/index.php
@@ -1,19 +1,23 @@
 <?php get_header(); ?>
+	<main class="default-page">
+		<?php while (have_posts()) : the_post(); ?>
 
-	
-		<!-- section -->
-		<section>
-
-
-
-			<?php get_template_part('loop'); ?>
-
-
-
-		</section>
-		<!-- /section -->
-
-
-
-
+			<?php if( have_rows('hero_section') ):?>
+			  <?php while ( have_rows('hero_section') ) : the_row();?>
+			    <?php $backgroundImage = get_sub_field('image');?>
+			    <section class="hero-section background-image-section"
+						style="background: url('<?php echo $backgroundImage[url];?>')">
+		        <div class="content">
+		          <h1 class="c-width-50"><?php the_sub_field('headline');?></h1>
+		        </div>
+			    </section>
+			  <?php endwhile;?>
+			<?php endif;?>
+	    <section class="default-section">
+        <div class="content">
+					<?php the_content(); ?>
+        </div>
+	    </section>
+		<?php endwhile; ?>
+	</main>
 <?php get_footer(); ?>
